4faaafb2f6d7c8c4870c41eb6969489a675c644d
Tom Cookson <twcookson@gmail.com>
2023-04-14 11:17:00 -0400

    working with GPT4 on design, had to make a few code adjustments myself

--- parkezai/src/components/Navigation.js
+++ parkezai/src/components/Navigation.js
@@ -1,42 +1,58 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import theme from '../theme';
 
 const StyledNav = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #1c1c1c;
-  padding: 1rem;
+  background-color: ${theme.primary};
+  padding: 0rem;
 `;
 
 const StyledUl = styled.ul`
   display: flex;
   justify-content: space-between;
   list-style-type: none;
-  margin: 0;
+  margin: 0em;
   padding: 0;
 `;
 
 const StyledLi = styled.li`
-  margin-left: 1rem;
+  display: flex;
+  margin-right: 1rem;
+  height: 100%;
 `;
 
 const StyledLink = styled(Link)`
   text-decoration: none;
-  color: #ffffff;
+  color: ${theme.accent};
   font-weight: bold;
-  transition: color 0.3s;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding: 1rem 0.5rem; // Increase the padding values to make the buttons bigger
+`;
+
+const StyledButton = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: transparent;
+  height: 100%;
+  transition: background-color 0.3s;
 
   &:hover {
-    color: #ffc107;
+    background-color: ${theme.secondary};
   }
 `;
 
 const Logo = styled.div`
-  color: #ffffff;
+  color: ${theme.accent};
   font-size: 1.2rem;
   font-weight: bold;
+  padding-left: 1.5rem;
 `;
 
 const Navigation = () => {
@@ -45,22 +61,34 @@ const Navigation = () => {
       <Logo>ParkEzAI</Logo>
       <StyledUl>
         <StyledLi>
-          <StyledLink to="/">Home</StyledLink>
+          <StyledButton>
+            <StyledLink to="/">Home</StyledLink>
+          </StyledButton>
         </StyledLi>
         <StyledLi>
-          <StyledLink to="/find-parking">Find Parking</StyledLink>
+          <StyledButton>
+            <StyledLink to="/find-parking">Find Parking</StyledLink>
+          </StyledButton>
         </StyledLi>
         <StyledLi>
-          <StyledLink to="/about">About</StyledLink>
+          <StyledButton>
+            <StyledLink to="/about">About</StyledLink>
+          </StyledButton>
         </StyledLi>
         <StyledLi>
-          <StyledLink to="/help-center">Help Center</StyledLink>
+          <StyledButton>
+            <StyledLink to="/help-center">Help Center</StyledLink>
+          </StyledButton>
         </StyledLi>
         <StyledLi>
-          <StyledLink to="/login">Login</StyledLink>
+          <StyledButton>
+            <StyledLink to="/login">Login</StyledLink>
+          </StyledButton>
         </StyledLi>
         <StyledLi>
-          <StyledLink to="/signup">Sign Up</StyledLink>
+          <StyledButton>
+            <StyledLink to="/signup">Sign Up</StyledLink>
+          </StyledButton>
         </StyledLi>
       </StyledUl>
     </StyledNav>
