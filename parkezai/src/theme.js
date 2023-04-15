079956a447b83e7609d25bedeed5afa3b5fbf259
Tom Cookson <twcookson@gmail.com>
2023-04-14 21:34:18 -0400

    decided on color scheme

--- parkezai/src/theme.js
+++ parkezai/src/theme.js
@@ -1,41 +1,8 @@
 const theme = {
-    primary: '#1A535C',
-    secondary: '#4ECDC4',
-    accent: '#FF6B6B',
-    backgroundColor: '#F1F1F1',
+   primary: '#673ab7',
+   secondary: '#9575cd',
+   accent: '#ffffff',
+   backgroundColor: '#f5f5f5'
    };
   
   export default theme;
-
-  /**
-   * Primary: #1A535C
-Secondary: #4ECDC4
-Accent: #FF6B6B
-Primary: #2C3E50
-Secondary: #2980B9
-Accent: #F1C40F
-Primary: #34495E
-Secondary: #1ABC9C
-Accent: #E74C3C
-Primary: #3A539B
-Secondary: #6C7A89
-Accent: #F89406
-Primary: #2E4053
-Secondary: #52BE80
-Accent: #F39C12
-Primary: #7B241C
-Secondary: #1F618D
-Accent: #F4D03F
-Primary: #4A235A
-Secondary: #3498DB
-Accent: #F7DC6F
-Primary: #1F3542
-Secondary: #32A9D9
-Accent: #F29559
-Primary: #1E1F26
-Secondary: #FF6A5E
-Accent: #2DCCD3
-Primary: #0B3D91
-Secondary: #F1C232
-Accent: #E64A19
-   */
