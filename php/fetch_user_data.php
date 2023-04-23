6ec9e7fd38463abd9319aa36580ed2b0fed7e74a
Tom Cookson <twcookson@gmail.com>
2023-04-23 16:09:07 -0400

    forgot some PHP in my last commit

---
+++ php/fetch_user_data.php
@@ -0,0 +1,35 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
+
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die(json_encode(["error" => "Connection failed: " . $conn->connect_error]));
+}
+
+$data = json_decode(file_get_contents("php://input"), true);
+$user_id = $data["user_id"];
+
+$sql = "SELECT * FROM users WHERE id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $user_id);
+$stmt->execute();
+$result = $stmt->get_result();
+$user = $result->fetch_assoc();
+
+if ($user) {
+    unset($user["password"]);
+    echo json_encode(["success" => true, "user" => $user]);
+} else {
+    echo json_encode(["success" => false, "message" => "User not found"]);
+}
+
+$stmt->close();
+$conn->close();
+?>
