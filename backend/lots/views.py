a3b13f1804c7c2342dedca084557bdcdd5be9342
tc10815 <twcookson@gmail.com>
2023-07-01 11:51:39 -0400

    test

--- backend/lots/views.py
+++ backend/lots/views.py
@@ -8,18 +8,21 @@ from django.core.exceptions import ValidationError
 class ImageUploadView(APIView):
 
     def post(self, request, format=None):
+        passcode = request.data.get('passcode')
+        if passcode != 'lightsecurity':
+            return Response({'detail': 'Invalid passcode'}, status=status.HTTP_401_UNAUTHORIZED)
+        
         image = request.data.get('image')
-        folder_name = request.data.get('folder_name')
         # Check if there are more than 5 different folders
         distinct_folder_count = ImageUpload.objects.values('folder_name').distinct().count()
-        if distinct_folder_count >= 5 and not ImageUpload.objects.filter(folder_name=folder_name).exists():
-            return Response({'detail': 'Image not stored, more than 5 different folders.'}, status=400)
-        # Check if there are more than 5 images in the folder
-        image_count_in_folder = ImageUpload.objects.filter(folder_name=folder_name).count()
-        if image_count_in_folder >= 5:
-            # Delete the oldest image in the folder
-            ImageUpload.objects.filter(folder_name=folder_name).order_by('timestamp').first().delete()
+        if distinct_folder_count >= 5:
+            return Response({'detail': 'Image not stored, more than 5 different folders.'}, status=status.HTTP_400_BAD_REQUEST)
+        # Check if there are more than 5 images overall
+        image_count = ImageUpload.objects.count()
+        if image_count >= 5:
+            # Delete the oldest image overall
+            ImageUpload.objects.order_by('timestamp').first().delete()
         # Save the new image
-        image_upload = ImageUpload(image=image, folder_name=folder_name)
+        image_upload = ImageUpload(image=image)
         image_upload.save()
-        return Response({'detail': 'Image successfully stored.'}, status=201)
+        return Response({'detail': 'Image successfully stored.'}, status=status.HTTP_201_CREATED)
