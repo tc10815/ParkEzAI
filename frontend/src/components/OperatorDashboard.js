aaf34c2196ccfd565ea70eef226e6d02bf5a7ca6
tc10815 <twcookson@gmail.com>
2023-07-30 17:00:49 -0400

    Replaced dummy data in lot owner analysis with real data (for the first 2 items in dashboard

--- frontend/src/components/OperatorDashboard.js
+++ frontend/src/components/OperatorDashboard.js
@@ -80,7 +80,7 @@ function formatDate(inputdate){
   
   return (timestampEST.getMonth() + 1) + '/' + timestampEST.getDate() + '/' + timestampEST.getFullYear() + ' ' 
     + hour + ':' + String(timestampEST.getMinutes()).padStart(2, '0') + ampm;
-}
+};
 
 const OperatorDashboard = () => {
 
@@ -89,7 +89,12 @@ const OperatorDashboard = () => {
   const canvasRef = useRef(null);
   const [imageSrc, setImageSrc] = useState('');
   const [humanLabels, setHumanLabels] = useState('');
-  const [humanLabelsJson, setHumanLabelsJson] = useState({});
+  const [currentCarsParked, setCurrentCarsParked] = useState('');
+  const [maxCarsParked, setMaxCarsParked] = useState('');
+  const [avgCarsParked, setAvgCarsParked] = useState('');
+
+  const [carsParkedToday, setCarsParkedToday] = useState('');
+
   const [spots, setSpots] = useState({});
   const [bestSpots, setBestSpots] = useState({});
   const [bestSpot, setBestSpot] = useState('');
@@ -121,11 +126,6 @@ const OperatorDashboard = () => {
       .then(data => {
           setSpots(data.spots);
           setBestSpots(data.bestspots);
-          setHumanLabelsJson(data.human_labels);
-          const trueLabels = Object.entries(data.human_labels)
-                      .filter(([key, value]) => value === true)
-                      .map(([key]) => key)
-                      .join(", ");
 
           let bestSpotString = 'None available';
           let BestSpotSoFarKey = 99999;
@@ -136,7 +136,42 @@ const OperatorDashboard = () => {
             }
           }
           setBestSpot(bestSpotString);
-          setHumanLabels(trueLabels);
+          let totalSpotsFull = 0;
+          for (let key in data.human_labels) {
+            if (data.human_labels[key]){
+              totalSpotsFull = totalSpotsFull + 1;
+            }
+          }
+
+          setCurrentCarsParked(totalSpotsFull);
+
+          const date = new Date();
+          const options = { weekday: 'long' };
+          const dayOfWeek = date.toLocaleDateString('en-US', options);
+          const hours = Array.from({ length: 24 }, (_, i) => i);
+          const half_hours = ['00', '30']
+          let totalCarsParkedEachHour = 0;
+          let totalImagesCounted = 0;
+          for (let hour in hours){
+            for (let half_hour in half_hours){
+              const key = dayOfWeek + ' ' + hour + ':' + half_hours[half_hour];
+              if (data.week_history[key]['cars'] != -1){
+                const keys = Object.keys(data.week_history[key]['cars']);
+                for (let inner_key of keys){
+                  if(data.week_history[key]['cars'][inner_key]){
+                    totalCarsParkedEachHour = totalCarsParkedEachHour + 1;
+                  }
+                  totalImagesCounted = totalImagesCounted + 1;
+                }
+              } 
+
+            }  
+          }
+          setCarsParkedToday(totalCarsParkedEachHour);
+          setAvgCarsParked(totalCarsParkedEachHour / totalImagesCounted);
+                
+
+          setMaxCarsParked(Object.keys(data.human_labels).length)
           setHumanTime(formatDate(data.timestamp));
           setImageSrc(API_URL + 'lots' + data.image_url);  // prefix the image URL with the server base URL and 'lots'
           setPreviousImageName(data.previous_image_name_part);
@@ -154,13 +189,13 @@ const OperatorDashboard = () => {
               const width = x2 - x1;
               const height = y2 - y1;
           
-              if(key === bestSpotString){
+              if (key === bestSpotString){
                   context.strokeStyle = 'green';
                   context.fillStyle = 'green';
-                }else if(data.human_labels[key]){
+              } else if (data.human_labels[key]){
                   context.strokeStyle = 'red';
                   context.fillStyle = 'red';
-              }else{
+              } else {
                   context.strokeStyle = 'blue';
                   context.fillStyle = 'blue';
               }
@@ -193,15 +228,15 @@ const OperatorDashboard = () => {
           <MyTable>
             <tr>
               <td>Current Occupancy</td>
-              <td>11/15</td>
+              <td>{currentCarsParked}/{maxCarsParked}</td>
             </tr>
             <tr>
-              <td>Cars Parked Today</td>
-              <td>142</td>
+              <td>Total Cars Parked Today Each Hour</td>
+              <td>{carsParkedToday/2}</td>
             </tr>
             <tr>
               <td>Average Occupancy Today</td>
-              <td>8.3</td>
+              <td>{(avgCarsParked * 100).toFixed(1)}%</td>
             </tr>
             <tr>
               <td>Total Cars Parked Today</td>
